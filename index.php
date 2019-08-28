7241e1cf8b0238aecdb186f05da5341c02e52624
www-data <www-data@cuda.dcc.ufmg.br>
2019-08-28 19:38:24 -0300

    Corrected tab title. TODO: change favicon.ico to A (Angha) instead of BC (old bcmp)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
   <head>
 
-    <title>bcmp</title>
+    <title>Angha</title>
 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
