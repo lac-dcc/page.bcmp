57eda3865a27c3b929643c409d6d016de3860b35
Bruno Conde Kind <brunocondekind@hotmail.com>
2019-10-24 18:36:02 -0300

    now I can say it - mobile support added

--- src/home.js
+++ src/home.js
@@ -1,41 +1,22 @@
-//  // mas vou usar pq é mais legível;
-// async function funcaoBruno() {
-//     const response = await fetch('features.json');
-//     const data = await response.json();
-//     const tbody = document.createElement("tbody");
-//     for(let row of data) {
-//         const tr = document.createElement("tr");
-//         const td = document.createElement("td");
-//         td.innerText = row.name;
-//         tr.appendChild(td);
-//         tbody.appendChild(tr);
-//     }
-//     const bruno = document.getElementById("feature-table");
-//     const oldTbody = bruno.getElementsByTagName("tbody");
-//     if(oldTbody && oldTbody.length) {
-//         oldTbody[0].remove();
-//     }
-//     bruno.appendChild(tbody);
-// }
-// funcaoBruno();
 
+// hides the charts from the "show more" sections
+$(".div-hidden-expandable").hide()
 
-$('.label-expand').click( (jo) => {
-    console.log('weee')
-    // var a = $(this).parent().class;
-    // //$txt = $lbx.parent().find('.div-hidden-expandable').html();
-    // console.log(a)
+// if the layout changes, swap .next() for .siblings('.div-hidden-expandable')
+$('.label-expand').click(x => {
+  if ($(x.currentTarget).children('i').text() == 'expand_more')
+  {
+    $(x.currentTarget).children('i').text('expand_less')
+  }
+  else
+  {
+    $(x.currentTarget).children('i').text('expand_more')
+  }
+  $(x.currentTarget).next().slideToggle(170)
+})
 
-    // //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-    // $txt.slideToggle(500, () => {
-    //     //execute this after slideToggle is done
-    //     console.log('uuu');
-    //     //change text of header based on visibility of content div
-    //     $lbx.text( () => {
-    //         //change text based on condition
-    //         console.log('weee');
-    //         return $content.is(":visible") ? "Collapse" : "Expand";
-    //     });
-    // });
+// used for "show more" links
+$('.a-expand').click(x=>$(x.currentTarget).parent().next().slideToggle(170))
 
-});
+//
+// $('.icon-expand').click(x=>$(x.currentTarget).text('expand_less'))
