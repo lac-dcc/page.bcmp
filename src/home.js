2082a245991e55d4e1b7dd950679dfc0afcb779f
Bruno Conde Kind <brunocondekind@hotmail.com>
2019-10-26 15:43:45 -0300

    Charts split and added to the page, mobile looking good

--- src/home.js
+++ src/home.js
@@ -1,22 +1,26 @@
 
-// hides the charts from the "show more" sections
+// hides the content in "show more" sections when the page is first loaded
 $(".div-hidden-expandable").hide()
+
 
 // if the layout changes, swap .next() for .siblings('.div-hidden-expandable')
 $('.label-expand').click(x => {
-  if ($(x.currentTarget).children('i').text() == 'expand_more')
-  {
-    $(x.currentTarget).children('i').text('expand_less')
-  }
-  else
-  {
+
+  // prevents going to the top of the page when links with href="#" are clicked
+  x.preventDefault()
+
+  // toggles the 'expand/hide' icon
+  $(x.currentTarget).children('i').text() == 'expand_more' ?
+    $(x.currentTarget).children('i').text('expand_less')   :
     $(x.currentTarget).children('i').text('expand_more')
-  }
+
+  // toggle content visibility
   $(x.currentTarget).next().slideToggle(170)
 })
 
-// used for "show more" links
-$('.a-expand').click(x=>$(x.currentTarget).parent().next().slideToggle(170))
 
-//
-// $('.icon-expand').click(x=>$(x.currentTarget).text('expand_less'))
+// used for "show more" links
+$('.a-expand').click(x => {
+  x.preventDefault()
+  $(x.currentTarget).parent().next().toggle(0)
+})
